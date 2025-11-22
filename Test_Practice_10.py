fb8f6fc0236f74d910ea94bb73179c3b67efb0dd
vidyanand <vds246810@gmail.com>
2025-11-22 15:50:57 +0530

    commit3

---
+++ Test_Practice_10.py
@@ -0,0 +1,19 @@
+
+class Test_God:
+
+
+    def test_templeA(self):
+        print("temple_A")
+
+    def test_hometemple(self):
+        print("home_temple")
+
+    def test_humantemple(self):
+        print("humantemple")
+
+g = Test_God()
+g.test_hometemple()
+g.test_humantemple()
+g.test_templeA()
+
+
